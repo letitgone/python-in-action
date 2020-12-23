5b6b8c433951a024f1c04218e4cb2c2a8987df42
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2020-12-23 23:57:59 +0800

    ZhangGJ:List

--- list/List.py
+++ list/List.py
@@ -11,3 +11,49 @@ print(bicycles[-2])
 
 message = f"My fist bicycle was a {bicycles[0].title()}"
 print(message)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+motorcycles.insert(1, 'ducati')
+print(motorcycles)
+
+del motorcycles[0]
+print(motorcycles)
+
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+
+motorcycles.pop(1)
+print(motorcycles)
+
+motorcycles.remove('suzuki')
+print(motorcycles)
+
+# sort永久排序
+car = ['bmw', 'audi', 'toyota', 'subaru']
+car.sort()
+print(car)
+
+car.sort(reverse=True)
+print(car)
+print("---------------------------------------")
+
+# sorted临时排序
+car = ['bmw', 'audi', 'toyota', 'subaru']
+print(car)
+print(sorted(car))
+print(car)
+
+# 反转列表
+car.reverse()
+print(car)
+
+print(len(car))
