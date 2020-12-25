f2701ba08e0ebca5b340bac21b4f4c5677c6650c
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2020-12-25 23:23:08 +0800

    ZhangGJ:Dictionary

--- dictionary/Dictionary.py
+++ dictionary/Dictionary.py
@@ -40,3 +40,64 @@ for key, value in user_0.items():
 for key in user_0.keys():
     print(key.title())
 
+favorite_language = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+for key in sorted(favorite_language.keys()):
+    print(key)
+
+print("-----------------------------------------")
+for value in favorite_language.values():
+    print(value)
+print("-----------------------------------------")
+for value in set(favorite_language.values()):
+    print(value)
+
+alien_0 = {'color': 'green', 'points': 5}
+alien_1 = {'color': 'yellow', 'points': 10}
+alien_2 = {'color': 'red', 'points': 15}
+aliens = [alien_0, alien_1, alien_2]
+print("-----------------------------------------")
+aliens = []
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:5]:
+    print(alien)
+print('...')
+
+print(f"Total number of aliens: {len(aliens)}")
+
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushrooms', 'extra cheese'],
+}
+print(f"You ordered a {pizza['crust']}-crust pizza"
+      f"with the following toppings:")
+for topping in pizza['toppings']:
+    print("\t" + topping)
+
+users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+    },
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+    }
+}
+
+for username, user_info in users.items():
+    print(f"\nUsername: {username}")
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
