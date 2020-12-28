e1d47ccca1ad1fd94f36d2c6e4584b3d79351d71
ZhangGJ <48301286+letitgone@users.noreply.github.com>
2020-12-28 23:24:48 +0800

    ZhangGJ:Function

---
+++ function/make_pizzas.py
@@ -0,0 +1,14 @@
+# @Author ZhangGJ
+# @Date 2020/12/28 23:11
+
+from pizza import make_pizza
+import person
+import function as fu
+from formatted_name import get_formatted_name as name
+from greeter import *
+
+print(make_pizza('test'))
+print(person.build_person('test1', 'test2'))
+print(fu.person)
+print(name('test1', 'test2'))
+print(get_formatted_name('test1', 'test2'))
