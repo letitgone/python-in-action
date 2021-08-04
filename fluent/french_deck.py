6c9466dbc33be129a537b619177aa51ab59e7b92
Gavin <48301286+letitgone@users.noreply.github.com>
2021-08-04 10:31:41 +0800

    Gavin:Fluent python

--- fluent/french_deck.py
+++ fluent/french_deck.py
@@ -2,6 +2,7 @@
 # @Date 2021/07/29 15:30
 
 import collections
+from random import choice
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
@@ -22,5 +23,37 @@ class FrenchDeck:
 
 
 if __name__ == '__main__':
+    beer_card = Card('7', 'diamonds')
+    print(beer_card)
+
     deck = FrenchDeck()
-    len(deck)
+    print(len(deck))
+
+    print(deck[0])
+    print(deck[-1])
+
+    print(choice(deck))
+
+    print(deck[:3])
+    print(deck[12::13])
+    print('----------------------------------------')
+    for card in deck:
+        print(card)
+    print('----------------------------------------')
+    for card in reversed(deck):
+        print(card)
+    print('----------------------------------------')
+
+    print(Card('Q', 'hearts') in deck)
+    print(Card('7', 'beasts') in deck)
+
+    suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+
+    def spades_high(card):
+        rank_value = FrenchDeck.ranks.index(card.rank)
+        return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+    for card in sorted(deck, key=spades_high):
+        print(card)
