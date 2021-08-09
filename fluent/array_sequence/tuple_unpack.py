e99d1c5ef52ff546bb0deca9cc562ade76be95f9
Gavin <48301286+letitgone@users.noreply.github.com>
2021-08-09 16:46:23 +0800

    Gavin:Tuple

--- fluent/array_sequence/tuple_unpack.py
+++ fluent/array_sequence/tuple_unpack.py
@@ -1,5 +1,8 @@
 # @Author ZhangGJ
 # @Date 2021/08/09 10:00
+
+import os
+
 lax_coordinates = (33.234, -118.2342)
 latitude, longitude = lax_coordinates
 print(latitude)
@@ -13,8 +16,6 @@ print(divmod(*t))
 quotient, remainder = divmod(*t)
 print(quotient)
 print(remainder)
-
-import os
 
 _, filename = os.path.split('/Users/zhanggj/Downloads/development-environment')
 print(filename)
@@ -30,3 +31,17 @@ a, *body, c, d = range(5)
 print(a, body, c, d)
 *head, b, c, d = range(5)
 print(head, b, c, d)
+
+metro_areas = [
+    ('Tokyo', 'JP', '36.933', (35.345, 139.3432)),
+    ('Delhi Ncp', 'IN', '21.933', (328.345, 77.3432)),
+    ('Mexico City', 'MX', '20.933', (19.345, -99.3432)),
+    ('New York-Newark', 'US', '20.933', (40.345, -74.3432)),
+    ('Sao Paulo', 'BR', '19.933', (-23.345, -46.3432)),
+]
+
+print('{:15} | {:^9} | {:^9}'.format('', 'lat', 'long.'))
+fmt = '{:15} | {:9.4f} | {:9.4f}'
+for name, cc, pop, (latitude, longitude) in metro_areas:
+    if longitude <= 0:
+        print(fmt.format(name, latitude, longitude))
